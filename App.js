6483272a73d302f20b61cd6022b587e5577cd91e
alestor123 <alestoraldous@gmail.com>
2021-01-08 16:02:22 +0530

    added main code

--- App.js
+++ App.js
@@ -1,4 +1,12 @@
 var axios = require('axios');
 module.exports = async name =>{
 if(!name) throw Error('Name not found')
+var year = await axios.get(`https://api.npmjs.org/downloads/point/last-year/${name}`)
+var week = await axios.get(`https://api.npmjs.org/downloads/point/last-week/${name}`)
+var day = await axios.get(`https://api.npmjs.org/downloads/point/last-day/${name}`)
+return {
+yesterday:day.data.downloads,
+lastweek:week.data.downloads,
+lastyear:year.data.downloads
+}
 }
