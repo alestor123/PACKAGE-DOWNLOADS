10f1a16e70d3bd76c43af2b2125f0344b817cf2e
alestor123 <alestoraldous@gmail.com>
2021-01-08 16:10:11 +0530

    added main code

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var pkd = require('./App');
+
+(async () => {
+var data = await pkd(process.argv[2] || 'github');
+console.log('\n Last Day : ' + data.yesterday + ' Downloads \n '+ '\n Last Week : ' + data.lastweek + ' Downloads \n ' + '\n Last Year : ' + data.lastyear + ' Downloads \n ')
+})();
